da100a4cb2cec2b3aa18b687c758b09c209814f7
Philipp Cuntz <philipp@cuntz.org>
2025-01-11 11:11:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 MODBUS_CLIENT_DIR="/opt/victronenergy/dbus-modbus-client"
-MODULE_URL="https://github.com/Kartax/dbus-modbus-client-kostal-smartmeter/blob/main/Kostal_SmartEnergyMeter.py"
+MODULE_URL="https://raw.githubusercontent.com/Kartax/dbus-modbus-client-kostal-smartmeter/main/Kostal_SmartEnergyMeter.py"
 FILE_TO_EDIT="dbus-modbus-client.py"
 CACHE_PATH="$MODBUS_CLIENT_DIR/__pycache__/dbus-modbus-client.cpython-38.pyc"
 
-echo "Navigating to the modbus client folder..."
-cd $MODBUS_CLIENT_DIR || { echo "Directory $MODBUS_CLIENT_DIR not found!"; exit 1; }
+echo "Navigating to $MODBUS_CLIENT_DIR..."
+cd "$MODBUS_CLIENT_DIR" || { echo "Failed to navigate to $MODBUS_CLIENT_DIR. Exiting."; exit 1; }
 
-echo "Downloading Kostal_SmartEnergyMeter.py..."
-wget -q -O Kostal_SmartEnergyMeter.py "$MODULE_URL" || { echo "Failed to download the module!"; exit 1; }
+echo "Downloading Kostal_SmartEnergyMeter.py from $MODULE_URL..."
+wget -q -O Kostal_SmartEnergyMeter.py "$MODULE_URL" || { echo "Failed to download Kostal_SmartEnergyMeter.py. Exiting."; exit 1; }
 
-echo "Editing $FILE_TO_EDIT..."
+echo "Editing $FILE_TO_EDIT to add import statement..."
 if ! grep -q "import Kostal_SmartEnergyMeter" "$FILE_TO_EDIT"; then
-    sed -i "/^import/s/$/\\nimport Kostal_SmartEnergyMeter/" "$FILE_TO_EDIT" || { echo "Failed to modify the file!"; exit 1; }
+    sed -i "/^import/s/$/\\nimport Kostal_SmartEnergyMeter/" "$FILE_TO_EDIT" || { echo "Failed to modify $FILE_TO_EDIT. Exiting."; exit 1; }
+    echo "import Kostal_SmartEnergyMeter added to $FILE_TO_EDIT."
 else
-    echo "import Kostal_SmartEnergyMeter already present in $FILE_TO_EDIT."
+    echo "import Kostal_SmartEnergyMeter already exists in $FILE_TO_EDIT."
 fi
 
-echo "Deleting Python cache..."
-#rm -f $CACHE_PATH || { echo "Failed to delete Python cache!"; exit 1; }
+echo "Deleting Python cache at $CACHE_PATH..."
+rm -f "$CACHE_PATH" || { echo "Failed to delete Python cache. Exiting."; exit 1; }
 
-echo "Restarting the GX device..."
-#reboot
+echo "Rebooting the system..."
+reboot
