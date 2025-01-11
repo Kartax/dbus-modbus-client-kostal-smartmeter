e917b3730d1d50d914f31d067f936a425ea3d041
Philipp Cuntz <philipp@cuntz.org>
2025-01-11 11:32:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ CACHE_PATH="$MODBUS_CLIENT_DIR/__pycache__/dbus-modbus-client.cpython-38.pyc"
 echo "Navigating to $MODBUS_CLIENT_DIR..."
 cd "$MODBUS_CLIENT_DIR" || { echo "Failed to navigate to $MODBUS_CLIENT_DIR. Exiting."; exit 1; }
 
-echo "Downloading Kostal_SmartEnergyMeter.py from $MODULE_URL..."
+echo "Downloading Kostal_SmartEnergyMeter.py ..."
 wget -q -O Kostal_SmartEnergyMeter.py "$MODULE_URL" || { echo "Failed to download Kostal_SmartEnergyMeter.py. Exiting."; exit 1; }
 
-echo "Editing $FILE_TO_EDIT to add import statement..."
+echo "Checking if import Kostal_SmartEnergyMeter is already present in $FILE_TO_EDIT..."
 if ! grep -q "import Kostal_SmartEnergyMeter" "$FILE_TO_EDIT"; then
-    sed -i "/^import/s/$/\\nimport Kostal_SmartEnergyMeter/" "$FILE_TO_EDIT" || { echo "Failed to modify $FILE_TO_EDIT. Exiting."; exit 1; }
-    echo "import Kostal_SmartEnergyMeter added to $FILE_TO_EDIT."
+    echo "Adding import Kostal_SmartEnergyMeter before import logging..."
+    sed -i '/import logging/i import Kostal_SmartEnergyMeter' "$FILE_TO_EDIT" || { echo "Failed to modify $FILE_TO_EDIT. Exiting."; exit 1; }
 else
-    echo "import Kostal_SmartEnergyMeter already exists in $FILE_TO_EDIT."
+    echo "import Kostal_SmartEnergyMeter is already present in $FILE_TO_EDIT."
 fi
 
-echo "Deleting Python cache at $CACHE_PATH..."
+echo "Deleting Python cache..."
 rm -f "$CACHE_PATH" || { echo "Failed to delete Python cache. Exiting."; exit 1; }
 
 echo "Rebooting the system..."
